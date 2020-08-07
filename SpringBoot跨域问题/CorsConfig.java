8bbd7143b20101a14addb6a19434fc7001e62c98
谭龙跃 <44675278+tanlongyue@users.noreply.github.com>
2020-08-07 15:11:32 +0800

    Add files via upload

---
+++ SpringBoot跨域问题/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.gz.gy.yueyouth.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+//跨域请求配置类
+@Configuration
+public class CorsConfig {
+
+
+
+    //region  专门用于处理解决前端发送请求过来时候的跨域问题
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        corsConfiguration.setMaxAge(180000L);// 4.解决跨域请求两次，预检请求的有效期，单位为秒
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+    //endregion
+
+
+
+}
